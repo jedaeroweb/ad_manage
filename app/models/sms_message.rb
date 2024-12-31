f001d370ad49a03aa199d3b0344fba5becf87907
sleepinglion <admin@sleepinglion.pe.kr>
2024-12-31 20:31:58 +0900

    modified:   app/models/ad_comment.rb 	modified:   app/models/sms_message.rb 	modified:   config/environments/development.rb

--- app/models/sms_message.rb
+++ app/models/sms_message.rb
@@ -1,6 +1,4 @@
 class SmsMessage < ActiveRecord::Base
-  attr_accessible :msg_callback, :msg_charset, :msg_lasttime, :msg_phone, :msg_priority, :msg_rsltcode, :msg_rslttime, :msg_sendtime, :msg_status, :msg_text
-
   def self.next_message_to_send
     SmsMessage.first(:conditions => ["msg_rslttime IS NULL AND msg_sendtime <= ?", Time.now.utc], :order => "id DESC")
   end
